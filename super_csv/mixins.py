e5c5bf05aedec1fc35ca79b30ec2d2bf97e4db75
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-01-19 21:53:30 +0500

    pyupgrade cleanup.

--- super_csv/mixins.py
+++ super_csv/mixins.py
@@ -186,7 +186,7 @@ class DeferrableMixin:
                     # current transaction.
                     operation = self.save()
             except DatabaseError:
-                log.exception(f'Error saving DeferrableMixin {self}')
+                log.exception('Error saving DeferrableMixin {}'.format(self))
                 raise
 
             # Now enqueue the async task.
