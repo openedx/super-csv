2f7093f278616e3b685e0183226fd6e767f33ce4
Matt Hughes <mhughes@edx.org>
2021-12-08 15:01:41 -0500

    refactor: update of translation function removed from django 4

--- super_csv/serializers.py
+++ super_csv/serializers.py
@@ -6,7 +6,7 @@ import logging
 
 import simplejson as json
 from django.contrib.auth import get_user_model
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 from .models import CSVOperation
